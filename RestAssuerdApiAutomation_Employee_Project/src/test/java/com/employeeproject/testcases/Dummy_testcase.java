63267c1a2c3096276f9535f6f1b002a449530c5b
rozariosumi <rozariosumi>
2019-07-27 23:41:48 -0500

    Adding a dummy testcase

---
+++ RestAssuerdApiAutomation_Employee_Project/src/test/java/com/employeeproject/testcases/Dummy_testcase.java
@@ -0,0 +1,14 @@
+package com.employeeproject.testcases;
+
+import com.employeeproject.Base.TestBase;
+
+public class Dummy_testcase extends TestBase{
+	
+	
+	
+	public void dummytestcaseTopush() {
+		
+		logger.info ("Dummy test cases");
+	}
+
+}
